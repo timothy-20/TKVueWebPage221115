a9536fbb4354e2043169f4dabfa5fea67e341c3f
timothy-20 <jungun3329@gmail.com>
2022-11-20 20:27:33 +0900

    221120) - App component 구조 수정 중. - aside, main content 세로로 길어질 경우 스크롤 처리가 되도록 구현 중.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,122 +1,37 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="header-section"><TKHeaderSection /></div>
-
-      <div class="content-section">
-        <div class="side-table-section"><TKSideTableSection /></div>
-        <div class="main-content-section"><HelloWorld msg="Welcome to Your Vue.js App"/></div>
+      <div class="header-wrapper">
+        Header wrapper
       </div>
+      <div class="main-wrapper">
+        <div class="aside-content">
+          <div class="test-content">
 
-      <div class="footer-section">
+          </div>
 
+        </div>
+
+        <div class="main-content">
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "@components/HelloWorld";
-import TKHeaderSection from "@components/TKMainHeaderSection";
-import TKSideTableSection from "@components/TKMainSideTableSection";
+// import HelloWorld from "@components/HelloWorld";
+// import TKHeaderSection from "@components/TKMainHeaderSection";
+// import TKSideTableSection from "@components/TKMainSideTableSection";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    TKHeaderSection,
-    TKSideTableSection,
+    // HelloWorld,
+    // TKHeaderSection,
+    // TKSideTableSection,
   }
 }
 </script>
 
-<style>
-@font-face {
-  font-family: "NotoSansKR-Black";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Black.otf") format("opentype");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Bold";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Bold.otf") format("opentype");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Light";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Light.otf") format("opentype");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Medium";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Medium.otf") format("opentype");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Regular";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Regular.otf") format("opentype");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Thin";
-  src: url("@assets/font/Noto_Sans_KR/NotoSansKR-Thin.otf") format("opentype");
-}
-
-* {
-  margin: 0 auto;
-  padding: 0;
-  border: 0 solid;
-  box-sizing: border-box;
-  outline: none;
-}
-
-#app {
-  --primary-bg-color: #343434;
-  --primary-border-color: #101010;
-  --primary-button-color: #000000;
-  --primary-font-color: #ffffff;
-  --primary-box-shadow-bottom: 0 1px 0 #262626, 0 1.5px 0 #333333, 0 2px 0 #363636;
-
-  font-family: NotoSansKR-Regular, sans-serif;
-  transform: rotate(0.04deg);
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-
-.container {
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-flow: column wrap;
-}
-
-.container > * {
-  width: inherit;
-}
-
-.header-section {
-  height: 5rem;
-  display: flex;
-  z-index: 9999;
-}
-
-.content-section {
-  flex-grow: 1;
-  display: flex;
-  flex-flow: row wrap;
-}
-
-.content-section .side-table-section {
-  min-width: 16rem;
-  display: flex;
-}
-
-.content-section .main-content-section {
-  flex-grow: 1;
-}
-
-.footer-section {
-  min-height: 6rem;
-  background-color: yellowgreen;
-}
-</style>
+<style src="./App.css" />
