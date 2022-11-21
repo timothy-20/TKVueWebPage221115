970489d0a84b8287d9b49bdd8c382d94400e3d44
timothy <jungun3329@gmail.com>
2022-11-21 17:10:05 +0900

    221121) - TKMainSideTableSection, table section 제외한 틀 구현 완료.

--- frontend/src/components/TKMainSideTableSection.vue
+++ frontend/src/components/TKMainSideTableSection.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="root">
     <div class="side-table-container">
-      <div class="title-wrapper">
-        <div class="title">
+      <section class="title">
+        <div class="title-wrapper">
           <span class="title-text">Untitled</span>
           <button class="title-extension" type="button">
-            <svg class="arrow-image" x="0" y="0" viewBox="0 0 100 300">
-              <polyline points="10 130, 50 170, 90 130" />
+            <svg class="arrow-image" x="0" y="0" viewBox="0 0 100 100">
+              <polyline points="10 30, 50 70, 90 30" />
             </svg>
           </button>
         </div>
-      </div>
+      </section>
 
       <div class="table-wrapper">
 
       </div>
 
-      <div class="user-info-wrapper">
+      <section class="footer">
+        <div class="footer-wrapper">
+          <div class="user-info-container">
+            <img src="@assets/image/person-icon.svg" alt="user profile image" class="user-profile" />
+            <div class="user-info">
+              <div class="user-name"><span>Unknown</span></div>
+              <div class="user-description"><span>unknown description</span></div>
+            </div>
+          </div>
 
-      </div>
+          <div class="button-container">
+            <button class="setting" type="button">
+              <div class="image-wrapper">
+                <img src="@assets/image/cogwheel-icon.svg" alt="setting icon" class="setting-image" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
